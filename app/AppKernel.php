b6c8878cd28aa93d600f08adb62630e1628c616a
Andrew Hilobok <hilobok@gmail.com>
2013-09-23 16:15:30 +0300

    Automatically register required bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -7,16 +7,18 @@ class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Modu\Bundle\SiteBundle\ModuSiteBundle(),
+        $bundles = array_map(function($class) { return new $class; },
+            array_unique($this->processRequiredBundles(array(
+                'Symfony\Bundle\FrameworkBundle\FrameworkBundle',
+                'Symfony\Bundle\SecurityBundle\SecurityBundle',
+                'Symfony\Bundle\TwigBundle\TwigBundle',
+                'Symfony\Bundle\MonologBundle\MonologBundle',
+                'Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle',
+                'Symfony\Bundle\AsseticBundle\AsseticBundle',
+                'Doctrine\Bundle\DoctrineBundle\DoctrineBundle',
+                'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle',
+                'Modu\Bundle\SiteBundle\ModuSiteBundle'
+            )))
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -31,5 +33,21 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    private function processRequiredBundles($requiredBundles)
+    {
+        foreach ($requiredBundles as $class) {
+            $callable = sprintf('%s::getRequiredBundles', $class);
+
+            if (is_callable($callable)) {
+                $requiredBundles = array_merge(
+                    $requiredBundles,
+                    $this->processRequiredBundles(call_user_func($callable))
+                );
+            }
+        }
+
+        return $requiredBundles;
     }
 }
