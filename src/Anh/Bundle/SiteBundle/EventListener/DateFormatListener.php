a4755aa12638c5624ef83188b95918364e7f0aca
Andrew Hilobok <hilobok@gmail.com>
2013-10-23 14:14:36 +0300

    Added DateFormatListener

---
+++ src/Anh/Bundle/SiteBundle/EventListener/DateFormatListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Anh\Bundle\SiteBundle\EventListener;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class DateFormatListener
+{
+    protected $container;
+
+    /**
+     * Constructor
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Set the default date format for twig
+     *
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $dateTimeFormat = $this->container->getParameter('anh_site.date_format');
+        $this->container->get('twig')->getExtension('core')->setDateFormat($dateTimeFormat, '%d days');
+    }
+}
