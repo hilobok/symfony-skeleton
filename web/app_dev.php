a175b1b75ccf5da40fd5e66387effe2c4432ffc4
Andrew Hilobok <hilobok@gmail.com>
2014-07-28 16:55:05 +0300

    Synced with symfony/symfony-standard v2.5.2

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,9 +3,16 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-if ($_SERVER['SERVER_NAME'] != 'dev.hostname.com') {
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+//umask(0000);
+
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+if (@$_SERVER['SERVER_NAME'] !== 'dev.hostname.com')
+{
     header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file.');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
